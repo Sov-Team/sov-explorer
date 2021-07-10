5be83de0c8a14ddf95fe9a0653f3ac7b72e5f46e
Sylvain Goumy <goumsss@protonmail.com>
2021-07-10 18:25:17 +0200

    :speech_balloon: update footer link

--- src/config/content.js
+++ src/config/content.js
@@ -2,8 +2,8 @@ export default {
   footer: [
     'The Sovryn explorer is currently being developed for the Sovrython',
     {
-      text: 'Learn more about Sovryn, the Bitcoin-native DeFi',
-      link: 'https://www.sovryn.app/'
+      text: 'Fork us on github',
+      link: 'https://github.com/Sov-Team/sov-explorer'
     }
   ]
 }
